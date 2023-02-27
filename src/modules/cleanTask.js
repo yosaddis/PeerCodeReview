d8c7389ad4c36f2bb5c5f8911ec0f52e926d8666
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-02-27 22:33:10 +0300

    Remove files form main

--- src/modules/cleanTask.js
+++
@@ -1,7 +0,0 @@
-const cleanTasks = (taskList) => {
-  const result = taskList.tasks.filter((task) => task.completed === false);
-  taskList.tasks = result;
-  taskList.createList();
-};
-
-export default cleanTasks;
